8605b751fa82a0107f35d26459411e2aa2768d4e
Taras Mankovski <tarasm@gmail.com>
2019-08-28 12:35:09 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,8 +1,9 @@
-if (process.env.npm_package_name === 'pseudomap' && process.env.npm_lifecycle_script === 'test') {
-    process.env.TEST_PSEUDOMAP = 'true'
+var env = process.env;
+if (env.npm_package_name === 'pseudomap' && env.npm_lifecycle_script === 'test') {
+    env.TEST_PSEUDOMAP = 'true'
 }
 
-if (typeof Map === 'function' && !process.env.TEST_PSEUDOMAP) {
+if (typeof Map === 'function' && !env.TEST_PSEUDOMAP) {
   module.exports = Map
 } else {
   module.exports = require('./pseudomap')
